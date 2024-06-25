c2da5428bb4e694bd249010d99daf7348c9afb41
aleanrocha <alean.oficial.me@gmail.com>
2024-06-25 16:48:02 -0300

    inference e annotation

---
+++ 02-fundamentos/02-inference-e-annotation/script.ts
@@ -0,0 +1,19 @@
+// INFERENCE E ANNOTATION
+
+/*
+  INFERENCE => É quando o TS indentifica e define o tipo de dado para nós.
+  ANNOTATION => É quando o tipo de dado manualmente.
+*/
+
+// Exemplo de Annotation
+
+let myName: string = 'Zezinho' // Definimos o tipo manualmente
+
+console.log(myName)
+
+// Exemplo de Inference
+
+let age = 19 // O própio TS vai definir o tipo com base no valor
+
+console.log(age)
+
