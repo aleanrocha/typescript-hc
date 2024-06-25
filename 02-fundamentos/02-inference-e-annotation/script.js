21108d132aecab45ba73d875734adec8c079ff90
aleanrocha <alean.oficial.me@gmail.com>
2024-06-25 17:01:42 -0300

    adicionar arquivo de configuracao ts

--- 02-fundamentos/02-inference-e-annotation/script.js
+++ 02-fundamentos/02-inference-e-annotation/script.js
@@ -1,11 +1,12 @@
+"use strict";
 // INFERENCE E ANNOTATION
 /*
   INFERENCE => É quando o TS indentifica e define o tipo de dado para nós.
   ANNOTATION => É quando o tipo de dado manualmente.
 */
 // Exemplo de Annotation
-var myName = 'Zezinho'; // Definimos o tipo manualmente
+let myName = 'Zezinho'; // Definimos o tipo manualmente
 console.log(myName);
 // Exemplo de Inference
-var age = 19; // O própio TS vai definir o tipo com base no valor
+let age = 19; // O própio TS vai definir o tipo com base no valor
 console.log(age);
