be8295c54f133ce08af6bac05cde3a3f6c3a7219
aleanrocha <alean.oficial.me@gmail.com>
2024-06-26 12:25:00 -0300

    desafio 01

---
+++ desafios/01/index.js
@@ -0,0 +1,8 @@
+// 1 - Criar uma função que aceite 2 argumentos do tipo number
+// 2 - Exibir a soma entre eles
+var num1 = 10;
+var num2 = 17;
+var sum = function (n1, n2) {
+    console.log("\u00C0 soma de ".concat(n1, " + ").concat(n2, " \u00E9 ").concat(n1 + n2));
+};
+sum(num1, num2);
---
+++ desafios/01/index.ts
@@ -0,0 +1,11 @@
+// 1 - Criar uma função que aceite 2 argumentos do tipo number
+// 2 - Exibir a soma entre eles
+
+const num1: number = 10
+const num2: number = 17
+
+const sum = (n1: number, n2: number) => {
+  console.log(`À soma de ${n1} + ${n2} é ${n1+n2}.`)
+}
+
+sum(num1, num2)
