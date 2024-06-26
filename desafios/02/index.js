de080ccf34efe0be286e7b12ba325f5fbb13ec39
aleanrocha <alean.oficial.me@gmail.com>
2024-06-26 12:41:59 -0300

    desafio 02

---
+++ desafios/02/index.js
@@ -0,0 +1,7 @@
+// 1 - Criar variável que recebe um número
+// 2 - Converta este número em string em uma nova variável
+// 3 - Essa variável de conversão deve estar tipada por inferência
+// 4 - Imprima o número na tela
+var num = 10;
+var numAsString = num.toString();
+console.log(numAsString, typeof numAsString);
---
+++ desafios/02/index.ts
@@ -0,0 +1,12 @@
+// 1 - Criar variável que recebe um número
+// 2 - Converta este número em string em uma nova variável
+// 3 - Essa variável de conversão deve estar tipada por inferência
+// 4 - Imprima o número na tela
+
+const num: number = 10
+
+const numAsString = num.toString()
+
+console.log(numAsString, typeof numAsString)
+
+
