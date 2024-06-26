f009fba0b6dd47b63f3d0cf927318d38a22f0269
aleanrocha <alean.oficial.me@gmail.com>
2024-06-26 15:15:34 -0300

    tipos em funcoes

--- 03-avancando-em-tipos/script.js
+++ 03-avancando-em-tipos/script.js
@@ -17,3 +17,22 @@ console.log('-------------------------------ANY--------------------------------'
 const items = [7, true, {}, 'Manga'];
 items.push([80, 90, 100]);
 console.log(items, typeof items);
+// TIPOS EM FUNÇÕES 
+console.log('-------------------------------TIPOS EM FUNÇÕES--------------------------------');
+function sum(n1, n2) {
+    console.log(`A soma de ${n1} + ${n1} é ${n1 + n2}.`);
+}
+sum(10, 10);
+// sum('a','b')
+// tipando o retorno das funções
+function greeting(name) {
+    return `Olá, ${name}.`;
+}
+console.log(greeting('João'));
+// console.log(greeting(332))
+// Funções anónimas
+setTimeout(() => {
+    let number = 100;
+    // number = 'oi'
+    console.log(number);
+}, 1000);
