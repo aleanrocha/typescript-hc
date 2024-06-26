18f7f27f306c6838cbebd7cffa5aeaf2fd7bdee0
aleanrocha <alean.oficial.me@gmail.com>
2024-06-26 10:05:29 -0300

    avancando em tipos - tipo array

---
+++ 03-avancando-em-tipos/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// ARRAYS
+const numbers = [2, 3, 4, 5, 7,];
+// numbers.push('8') // só aceita números
+numbers.push(8);
+console.log(numbers, typeof numbers);
+const names = ['João', 'Maria', 'Paulo', 'Pedro'];
+names.push('Lucas');
+console.log(names, typeof names);
+// OUTRA SINTAXE DE ARRAY
+const nums = [1000, 2000, 3000];
+const animals = ['Cat', 'Dog'];
+console.log(nums, typeof nums);
+console.log(animals, typeof animals);
---
+++ 03-avancando-em-tipos/script.ts
@@ -0,0 +1,24 @@
+// ARRAYS
+
+const numbers: number[] = [2,3,4,5,7,]
+
+// numbers.push('8') // só aceita números
+numbers.push(8)
+
+console.log(numbers, typeof numbers)
+
+const names: string[] = ['João', 'Maria', 'Paulo', 'Pedro']
+
+names.push('Lucas')
+
+console.log(names, typeof names)
+
+// OUTRA SINTAXE DE ARRAY
+
+const nums: Array<number> = [1000, 2000, 3000]
+const animals: Array<string> = ['Cat', 'Dog']
+
+console.log(nums, typeof nums)
+console.log(animals, typeof animals)
+
+
