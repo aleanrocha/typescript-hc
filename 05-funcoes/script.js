fe50ed79965a1b05ae73eb299d37ffcd657a5d63
aleanrocha <alean.oficial.me@gmail.com>
2024-06-29 15:51:28 -0300

    funcoes - funcao sem retorno

---
+++ 05-funcoes/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// FUNÇÕES
+// Função sem retorno
+// Podemos ter funções que não retornam valores e nesse caso utilizamos o VOID
+function whithoutReturn() {
+    console.log('Função sem retorno!');
+}
+whithoutReturn();
---
+++ 05-funcoes/script.ts
@@ -0,0 +1,10 @@
+// FUNÇÕES
+
+// Função sem retorno
+// Podemos ter funções que não retornam valores e nesse caso utilizamos o VOID
+
+function whithoutReturn():void {
+  console.log('Função sem retorno!')
+}
+
+whithoutReturn()
