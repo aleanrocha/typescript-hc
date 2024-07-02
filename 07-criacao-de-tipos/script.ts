0cd4f3c529ca7bdd842e06fef18bc806f84ffa4a
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-02 16:16:54 -0300

    criacao de tipos

---
+++ 07-criacao-de-tipos/script.ts
@@ -0,0 +1,7 @@
+// CRIAÇÃO DE TIPOS EM TYPESCRIPT
+
+/*
+  Há a possibilidade de gerar novos tipos em TS, como nos generics,
+  porém existem outros operadores que visam facilitar nossa vida
+  neste assunto.
+*/
