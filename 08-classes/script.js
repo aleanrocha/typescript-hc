ef5e24a4b030db59f6eaf5b951786d7c5a462d0d
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-05 09:39:50 -0300

    classes em ts

---
+++ 08-classes/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// CLASSES
+/*
+  Em TS podemos adicionar novos campos a uma classe, ou seja, iniciar
+  a classe com campos para os futuros dados dos objetos, que serão as
+  propiedades dos objetos instanciados, estes campos podem ser tipados
+  também, note que um campo declarado como vazio deve conter o sinal de !.
+*/ 
