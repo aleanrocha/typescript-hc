52f8fd40576735cedd197f909cf33f61caacd7f0
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-06 08:28:45 -0300

    variaveis com react e ts

--- 11-react-com-ts/react-&-ts/src/App.tsx
+++ 11-react-com-ts/react-&-ts/src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css'
-
 const App = () => {
+
+  // 01 - Variáveis
+
+  const name: string = 'Zezinho'
+  const age: number = 34
+  const isWorking: boolean = true
+  
   return (
     <>
      <h1>Hello React with TypeScript</h1>
+     <div>
+      <h2>Nome: {name}</h2>
+      <h2>Idade: {age}</h2>
+      {isWorking && <p>Está trabalhando!</p>}
+     </div>
     </>
   )
 }
