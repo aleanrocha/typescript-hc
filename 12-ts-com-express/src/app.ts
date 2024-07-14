222de772da6fe35c2db287ddfe706aed4d64732c
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-14 16:55:27 -0300

    iniciando express

--- 12-ts-com-express/src/app.ts
+++ 12-ts-com-express/src/app.ts
@@ -1,3 +1,17 @@
 // 01 - iniciando projeto
 
-console.log('TS + EXPRESS')
+// console.log('TS + EXPRESS')
+
+// 02 - init express
+
+
+import express from 'express'
+
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port} 🚀`))
