8d942a0ac359a1737f8de422ea38cc78e95658c8
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-15 22:07:51 -0300

    rota com post

--- 12-ts-com-express/src/app.ts
+++ 12-ts-com-express/src/app.ts
@@ -10,8 +10,19 @@ import express from 'express'
 const app = express()
 const port = 3001
 
+// Habilitando json
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// 03 - Rota POST
+
+app.post('/api/product', (req, res) => {
+  console.log(req.body)
+  return res.send({ message: 'Produto adicionado!' })
 })
 
 app.listen(port, () => console.log(`Server started on port ${port} 🚀`))
